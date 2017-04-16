9536c934cb34a3d501cbe03881c99569e5d2a2ae
ejabu <muhammadfahreza@gmail.com>
2017-04-17 02:10:02 +0700

    MDR done IDC done

--- ddc_1_0/models/doc_idc.py
+++ ddc_1_0/models/doc_idc.py
@@ -13,9 +13,35 @@ class doc_idc(models.Model):
     send_date = fields.Date(string='Sending Date', required=True)
     rece_date = fields.Date(string='Receiving Date',)
 
-    line_ids = fields.One2many('master.deliver', 'idc_id', 'MDR Line')
+    line_ids = fields.One2many('master.deliver', 'idc_id', 'MDR Line', ondelete='restrict')
+    state = fields.Selection(selection=[('new', 'New'), ('done', 'Done')])
 
     _defaults={
         'state': 'new',
         'send_date': lambda *a:datetime.now().strftime('%Y-%m-%d'),
     }
+
+    # @api.onchange('name')
+    # def update_name(self):
+    #     import ipdb; ipdb.set_trace()
+    #     # print "welcome"
+
+
+    @api.multi
+    def send_doc(self):
+        import ipdb; ipdb.set_trace()
+        for line_id in self.line_ids:
+
+            line_id.idc_number = ""
+            line_id.send_date = ""
+            line_id.rece_date = ""
+
+            line_id.doc_status = line_id.doc_status_update
+            line_id.rev_num = line_id.rev_num_update
+            line_id.state = 'done'
+            new_doc = line_id.copy()
+
+            line_id.idc_number = self.name
+            line_id.send_date = self.send_date
+            line_id.rece_date = self.rece_date
+        # self.state='done'
--- ddc_1_0/models/master_deliver.py
+++ ddc_1_0/models/master_deliver.py
@@ -6,37 +6,39 @@ class master_deliver(models.Model):
 
     _name= "master.deliver"
 
-    discipline = fields.Many2one('conf.discipline', 'Discipline', required=True)
-    doc_categ = fields.Many2one('conf.doc.categ', 'Category')
-    doc_sub = fields.Many2one('conf.doc.sub', 'Subsystem')
-    name = fields.Char(string='Document Number', required=True)
-    doc_title = fields.Char(string='Title')
-    doc_type = fields.Many2one('conf.doc.type', 'Document Type', required=True)
-    doc_type_des = fields.Char(string='Type Description')
+    discipline = fields.Many2one('conf.discipline', 'Discipline', required=True, ondelete='restrict', copy=True)
+    doc_categ = fields.Many2one('conf.doc.categ', 'Category', ondelete='restrict', copy=True)
+    doc_sub = fields.Many2one('conf.doc.sub', 'Subsystem', ondelete='restrict', copy=True)
+    name = fields.Char(string='Document Number', required=True, copy=True)
+    doc_title = fields.Char(string='Title', copy=True)
+    doc_type = fields.Many2one('conf.doc.type', 'Document Type', required=True, ondelete='restrict', copy=True)
+    doc_type_des = fields.Char(string='Type Description', copy=True)
 
-    sched_date = fields.Date(string='Schedule Date')
-    originator = fields.Many2one('res.partner', string='Originator')
-    internal_status = fields.Many2one('conf.internal.status', 'Internal Status')
+    sched_date = fields.Date(string='Schedule Date', copy=True)
+    originator = fields.Many2one('res.partner', string='Originator', copy=True)
 
-    doc_pred = fields.Char(string='Document Predecessor')
-    alt_doc = fields.Char(string='Alternative Document #')
+    doc_status = fields.Many2one('conf.doc.status', 'Status', ondelete='restrict', copy=True)
+    doc_status_update = fields.Many2one('conf.doc.status', 'Status', ondelete='restrict', copy=True)
+
+    # internal_status = fields.Many2one('conf.internal.status', 'Internal Status')
+
+    doc_pred = fields.Char(string='Document Predecessor', copy=True)
+    alt_doc = fields.Char(string='Alternative Document #', copy=True)
 
 
-    notes = fields.Text(string='Notes')
+    notes = fields.Text(string='Notes', copy=True)
     state = fields.Selection(selection=[('new', 'New'), ('done', 'Done')])
 
-    idc_id = fields.Many2one('doc.idc', 'Related IDC')
-    rev_num = fields.Many2one('conf.rev.num', 'Revision Number')
+    idc_id = fields.Many2one('doc.idc', 'Related IDC', ondelete='restrict', copy=False)
+    rev_num = fields.Many2one('conf.rev.num', 'Revision Number', ondelete='restrict', copy=True)
+    rev_num_update = fields.Many2one('conf.rev.num', 'Revision Number', ondelete='restrict', copy=True)
 
-    idc_number = fields.Char(string='IDC Number', related='idc_id.name', store=True)
-    send_date = fields.Date(string='Sending Date', related='idc_id.send_date', store=True)
-    rece_date = fields.Date(string='Receiving Date', related='idc_id.rece_date', store=True)
+    idc_number = fields.Char(string='IDC Number', copy=True)
+    send_date = fields.Date(string='Sending Date', copy=True)
+    rece_date = fields.Date(string='Receiving Date', copy=True)
 
     # file_name = fields.Char(string='File Name')
-    # doc_status = fields.Many2one('conf.doc.status', 'Status')
-    # doc_status_update = fields.Many2one('conf.doc.status', 'Status')
-    # rev_num = fields.Integer('Revision Number')
-    # rev_num_update = fields.Integer('Revision Number')
+
     # external_status = fields.Many2one('conf.external.status', 'External Status')
     # old_rev = fields.Char(string='Old Revision')
 
@@ -47,6 +49,11 @@ class master_deliver(models.Model):
     # send_eng_id = fields.Many2many('send.eng.doc', 'master_to_send_eng', 'master_deliver_id', 'send_eng_id', string="Master Deliver", copy=False)
     # rec_eng_id = fields.Many2many('rec.eng.doc', 'master_to_rec_eng', 'master_deliver_id', 'rec_eng_id', string="Master Deliver", copy=False)
 
-    _defaults={
+    _defaults = {
         'state': 'new',
     }
+    #
+    _sql_constraints = [
+        ('name', 'Check(1=1)', "The system record Can't be duplicate value for this field!")
+        # ('name', 'unique(name)', "The system record Can't be duplicate value for this field!")
+    ]

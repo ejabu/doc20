467dac5bf4a57e8e51feb91cb3826192d2dae17a
ejabu <muhammadfahreza@gmail.com>
2017-04-16 20:37:42 +0700

    Update 1st Form

--- ddc_1_0/models/master_deliver.py
+++ ddc_1_0/models/master_deliver.py
@@ -7,31 +7,38 @@ class master_deliver(models.Model):
     _name= "master.deliver"
 
     discipline = fields.Many2one('conf.discipline', 'Discipline', required=True)
-    doc_categ = fields.Many2one('conf.doc.categ', 'Document Category')
-    doc_sub = fields.Many2one('conf.doc.sub', 'Document Subsystem')
+    doc_categ = fields.Many2one('conf.doc.categ', 'Category')
+    doc_sub = fields.Many2one('conf.doc.sub', 'Subsystem')
     name = fields.Char(string='Document Number', required=True)
-    doc_title = fields.Char(string='Document Title')
-    originator = fields.Many2one('res.partner', string='Originator', required=True)
+    doc_title = fields.Char(string='Title')
+    doc_type = fields.Many2one('conf.doc.type', 'Document Type', required=True)
+    doc_type_des = fields.Char(string='Type Description')
+
+
+    sched_date = fields.Date(string='Schedule Date')
+    originator = fields.Many2one('res.partner', string='Originator')
+    internal_status = fields.Many2one('conf.internal.status', 'Internal Status')
 
     doc_pred = fields.Char(string='Document Predecessor')
-    file_name = fields.Char(string='File Name')
     alt_doc = fields.Char(string='Alternative Document #')
-    sched_date = fields.Date(string='Schedule Date')
-    doc_type = fields.Many2one('conf.doc.type', 'Document Type', required=True)
-    doc_status = fields.Many2one('conf.doc.status', 'Status')
-    doc_status_update = fields.Many2one('conf.doc.status', 'Status')
-    # rev_num = fields.Many2one('conf.rev.num', 'Revision Number')
-    rev_num = fields.Integer('Revision Number')
-    rev_num_update = fields.Integer('Revision Number')
-    internal_status = fields.Many2one('conf.internal.status', 'Internal Status', required=True)
-    external_status = fields.Many2one('conf.external.status', 'External Status')
-    old_rev = fields.Char(string='Old Revision')
-    notes = fields.Text(string='Notes')
 
+
+    notes = fields.Text(string='Notes')
     state = fields.Selection(selection=[('new', 'New'), ('done', 'Done')])
 
+
+    # file_name = fields.Char(string='File Name')
+    # doc_status = fields.Many2one('conf.doc.status', 'Status')
+    # doc_status_update = fields.Many2one('conf.doc.status', 'Status')
+    # # rev_num = fields.Many2one('conf.rev.num', 'Revision Number')
+    # rev_num = fields.Integer('Revision Number')
+    # rev_num_update = fields.Integer('Revision Number')
+    # external_status = fields.Many2one('conf.external.status', 'External Status')
+    # old_rev = fields.Char(string='Old Revision')
+
+
     # history_seq = fields.Char(string='History')
-    history_seq = fields.Char(string='History', required=True, copy=False, default=lambda self: self.env['ir.sequence'].next_by_code('master.deliver'))
+    # history_seq = fields.Char(string='History', required=True, copy=False, default=lambda self: self.env['ir.sequence'].next_by_code('master.deliver'))
 
     # send_eng_id = fields.Many2many('send.eng.doc', 'master_to_send_eng', 'master_deliver_id', 'send_eng_id', string="Master Deliver", copy=False)
     # rec_eng_id = fields.Many2many('rec.eng.doc', 'master_to_rec_eng', 'master_deliver_id', 'rec_eng_id', string="Master Deliver", copy=False)

a9cc6e2e7cbe9212dc07943eca26d9d9d68600e5
ejabu <muhammadfahreza@gmail.com>
2017-04-17 19:54:36 +0700

    Hide fields

--- ddc_1_0/models/master_deliver.py
+++ ddc_1_0/models/master_deliver.py
@@ -36,22 +36,41 @@ class master_deliver(models.Model):
     state = fields.Selection(selection=[('new', 'New'), ('done', 'Done')])
 
 
+    # idc_id = fields.Many2one('doc.idc', 'Related IDC', ondelete='restrict', copy=False)
+    # idc_number = fields.Char(string='IDC Number', related='idc_id.name', store=True, copy=False)
+    # send_date = fields.Date(string='IDC Sending Date', related='idc_id.send_date', store=True, copy=False)
+    # rece_date = fields.Date(string='IDC Receiving Date', related='idc_id.rece_date', store=True, copy=False)
+    #
+    # send_id = fields.Many2one('doc.send', 'Related Send', ondelete='restrict', copy=False)
+    # trans_number = fields.Char(string='Sending Transmittal Number', related='send_id.name', store=True, copy=False)
+    # trans_date = fields.Date(string='Transmittal Date', related='send_id.trans_date', store=True, copy=False)
+    # due_date = fields.Date(string='Due Date', related='send_id.due_date', store=True, copy=False)
+    # need_to_response = fields.Date(string='Need to Response', related='send_id.need_to_response', store=True, copy=False)
+    # antam_date = fields.Date(string='Antam Receive Date', related='send_id.antam_date', store=True, copy=False)
+    #
+    # rece_id = fields.Many2one('doc.rece', 'Related Receiving', ondelete='restrict', copy=False)
+    # recv_trans_number = fields.Char(string='Receiving Transmittal Number', related='rece_id.name', store=True, copy=False)
+    # recv_rece_date = fields.Date(string='Receiving Date', related='rece_id.recv_rece_date', store=True, copy=False)
+    # recv_comment = fields.Many2one('conf.rec.comment', 'Status Comment', ondelete='restrict', copy=False)
+
+
     idc_id = fields.Many2one('doc.idc', 'Related IDC', ondelete='restrict', copy=False)
-    idc_number = fields.Char(string='IDC Number', related='idc_id.name', store=True, copy=False)
-    send_date = fields.Date(string='IDC Sending Date', related='idc_id.send_date', store=True, copy=False)
-    rece_date = fields.Date(string='IDC Receiving Date', related='idc_id.rece_date', store=True, copy=False)
+    idc_number = fields.Char(string='IDC Number',copy=False)
+    send_date = fields.Date(string='IDC Sending Date', copy=False)
+    rece_date = fields.Date(string='IDC Receiving Date', copy=False)
 
     send_id = fields.Many2one('doc.send', 'Related Send', ondelete='restrict', copy=False)
-    trans_number = fields.Char(string='Sending Transmittal Number', related='send_id.name', store=True, copy=False)
-    trans_date = fields.Date(string='Transmittal Date', related='send_id.trans_date', store=True, copy=False)
-    due_date = fields.Date(string='Due Date', related='send_id.due_date', store=True, copy=False)
-    need_to_response = fields.Date(string='Need to Response', related='send_id.need_to_response', store=True, copy=False)
-    antam_date = fields.Date(string='Antam Receive Date', related='send_id.antam_date', store=True, copy=False)
+    trans_number = fields.Char(string='Sending Transmittal Number',  copy=False)
+    trans_date = fields.Date(string='Transmittal Date',  copy=False)
+    due_date = fields.Date(string='Due Date',  copy=False)
+    need_to_response = fields.Date(string='Need to Response',  copy=False)
+    antam_date = fields.Date(string='Antam Receive Date',  copy=False)
 
     rece_id = fields.Many2one('doc.rece', 'Related Receiving', ondelete='restrict', copy=False)
-    recv_trans_number = fields.Char(string='Receiving Transmittal Number', related='rece_id.name', store=True, copy=False)
-    recv_rece_date = fields.Date(string='Receiving Date', related='rece_id.recv_rece_date', store=True, copy=False)
+    recv_trans_number = fields.Char(string='Receiving Transmittal Number',  copy=False)
+    recv_rece_date = fields.Date(string='Receiving Date',  copy=False)
     recv_comment = fields.Many2one('conf.rec.comment', 'Status Comment', ondelete='restrict', copy=False)
+
 
     _defaults = {
         'state': 'new',

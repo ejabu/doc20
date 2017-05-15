0bd989d12c5ef34a2f92be1bd8f18ec78fe967dc
ejabu <muhammadfahreza@gmail.com>
2017-05-15 20:16:10 +0700

    Cleansing, Add a Status, Add a Revision

--- ddc_1_0/__openerp__.py
+++ ddc_1_0/__openerp__.py
@@ -7,25 +7,39 @@
     "description" : """Simple Document Management""",
     'data': [
         'static_lib.xml',
-        'dash/dash_discipline.xml',
-        'dash/dash_exstat.xml',
-        'dash/dash_delay_approve.xml',
-        'dash/dash_delay_approve_conf.xml',
-        'dash/dash_monthly.xml',
-        'dash/dash_monthly_conf.xml',
-        'dash/dash_pivot_monthly.xml',
-        'views/master_deliver.xml',
-        'views/master_deliver_report.xml',
-        'views/conf_rec_comment.xml',
-        'views/idc_in.xml',
-        'views/idc_out.xml',
-        'views/doc_send.xml',
-        'views/doc_send_client.xml',
-        'views/doc_rece.xml',
-        'views/doc_status_update.xml',
-        'views/doc_rev_update.xml',
-        'views/conf_master_data.xml',
-        'views/menus.xml',
+        #Reports
+            #Current Status
+                'dash/dash_discipline.xml',
+                'dash/dash_exstat.xml',
+                'dash/dash_monthly.xml',
+                'dash/dash_monthly_conf.xml',
+            #Performance
+                'dash/dash_delay_approve.xml',
+                'dash/dash_delay_approve_conf.xml',
+                'dash/dash_pivot_monthly.xml',
+            #MDR Overview
+                'views/master_deliver_report.xml',
+        #Master Document
+            #MDR
+                'views/master_deliver.xml',
+            #Update
+                'views/doc_rev_update.xml',
+                'views/doc_status_update.xml',
+
+        #Update
+            #IDC
+                'views/idc_in.xml',
+                'views/idc_out.xml',
+            #Transmittal
+                'views/doc_send.xml',
+                'views/doc_send_client.xml',
+                'views/doc_rece.xml',
+
+        #Configuration
+            'views/conf_master_data.xml',
+            'views/conf_rec_comment.xml',
+
+            'views/menus.xml',
     ],
     'demo':[
             #files containing demo data
--- ddc_1_0/static/src/js/o2m_add.js
+++ ddc_1_0/static/src/js/o2m_add.js
@@ -1,46 +1,30 @@
-/* Copyright 2016 Onestein
- * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
-odoo.define('web_x2many_delete_all.Mixin', function (require) {
+odoo.define('web_x2many_list_add_label.Mixin', function (require) {
     "use strict";
     var core = require('web.core');
 
-    var X2ManyListDeleteAllMixin = {
+    var X2manyListAddLabel = {
         events: {
             'click th.oe_list_record_delete': 'btn_delete_all_clicked'
         },
         reload_current_view: function() {
             var self = this;
             var res = this._super.apply(this, arguments);
-
-
             res.then(function() {
                 self.clearAdd();
-
             });
             return res
         },
         clearAdd: function() {
-            console.log('EEEEEEEEEEEEEE');
-            console.log(this.dataset.x2m.options);
             if (this.dataset.x2m.options && this.dataset.x2m.options.addLabel) {
                 this.$('.oe_form_field_x2many_list_row_add a').text(this.dataset.x2m.options.addLabel)
             }
-            // console.log(this.$('.oe_form_field_x2many_list_row_add a'));
-            // this.$('.oe_form_field_x2many_list_row_add a').text('hehe')
-            // this.$('.oe_form_field_x2many_list_row_add').remove()
         },
-        // btn_delete_all_clicked: function() {
-        //     if (this.get('effective_readonly')) return;
-        //     this.delete_all();
-        // },
-        // delete_all: function() {
-        //     this.viewmanager.views.list.controller.do_delete(this.dataset.ids);
-        // }
+
     }
 
-    core.form_widget_registry.get('many2many').include(X2ManyListDeleteAllMixin);
-    core.form_widget_registry.get('one2many').include(X2ManyListDeleteAllMixin);
+    core.form_widget_registry.get('many2many').include(X2manyListAddLabel);
+    core.form_widget_registry.get('one2many').include(X2manyListAddLabel);
 
-    return X2ManyListDeleteAllMixin;
+    return X2manyListAddLabel;
 });

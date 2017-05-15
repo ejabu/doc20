1d2bbeca0be3ddf271c34b988021fa863143d201
ejabu <muhammadfahreza@gmail.com>
2017-05-15 18:54:22 +0700

    10 hours work, Add an Item to Add

--- ddc_1_0/static/src/js/o2m_add.js
+++ ddc_1_0/static/src/js/o2m_add.js
@@ -1,94 +1,46 @@
-// (function () {
-//     "use strict";
-//     var form_relational = require('web.form_relational');
-//     console.log(form_relational);
-//     form_relational.X2ManyList.include({
-//         pad_table_to : function() {
-//             var self = this;
-//             this._super.apply(this, arguments);
-//             console.log('eja');
-//             console.log(this, arguments);
-//             self.$current.find('.oe_form_field_x2many_list_row_add a').text('Your Text');
-//         }
-//     });
-// }) ();
+/* Copyright 2016 Onestein
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
-// _____________________ done
-// odoo.define('hehe', function (require) {
-// 'use strict';
-//
-//     var core = require('web.core');
-//     console.log("core.view_registry");
-//     console.log(core.view_registry);
-//     var eja = core.view_registry.get('one2many')
-//     console.log(eja);
-//     var form_relational = require('web.form_relational');
-//     console.log(form_relational);
-// });
-
-odoo.define('hehe', function(require) {
-    'use strict';
-
+odoo.define('web_x2many_delete_all.Mixin', function (require) {
+    "use strict";
     var core = require('web.core');
-    var _t = core._t;
 
-    // var ListView = require('web.ListView');
-    //
-    // var X2ManyList = ListView.List.extend({
-    //     pad_table_to: function(count) {
-    //
-    //         if (!this.view.is_action_enabled('create') || this.view.x2m.get('effective_readonly')) {
-    //             this._super(count);
-    //             return;
-    //         }
-    //
-    //         this._super(count > 0
-    //             ? count - 1
-    //             : 0);
-    //
-    //         var self = this;
-    //         var columns = _(this.columns).filter(function(column) {
-    //             return column.invisible !== '1';
-    //         }).length;
-    //         if (this.options.selectable) {
-    //             columns++;
-    //         }
-    //         if (this.options.deletable) {
-    //             columns++;
-    //         }
-    //         console.log("this.$current");
-    //         console.log(this.$current);
-    //         console.log(this.$current.prev());
-    //         console.log(columns);
-    //         var $cell = $('<td>', {
-    //             colspan: columns,
-    //             'class': 'oe_form_field_x2many_list_row_add'
-    //         }).append($('<a>', {href: '#'}).text(_t("WOI")).click(function(e) {
-    //             e.preventDefault();
-    //             e.stopPropagation();
-    //             // FIXME: there should also be an API for that one
-    //             if (self.view.editor.form.__blur_timeout) {
-    //                 clearTimeout(self.view.editor.form.__blur_timeout);
-    //                 self.view.editor.form.__blur_timeout = false;
-    //             }
-    //             self.view.save_edition().done(function() {
-    //                 self.view.do_add_record();
-    //             });
-    //         }));
-    //
-    //         var $padding = this.$current.find('tr:not([data-id])');
-    //         // $(".oe_form_field_x2many_list_row_add").parent()[0].remove()
-    //
-    //         var $newrow = $('<tr>').append($cell);
-    //
-    //         $padding.eq(-1).remove();
-    //         if ($padding.length) {
-    //             $padding.before($newrow);
-    //         } else {
-    //             this.$current.append($newrow);
-    //         }
-    //     },
-    //
-    // });
-    // console.log('Loaded');
+    var X2ManyListDeleteAllMixin = {
+        events: {
+            'click th.oe_list_record_delete': 'btn_delete_all_clicked'
+        },
+        reload_current_view: function() {
+            var self = this;
+            var res = this._super.apply(this, arguments);
+
+
+            res.then(function() {
+                self.clearAdd();
+
+            });
+            return res
+        },
+        clearAdd: function() {
+            console.log('EEEEEEEEEEEEEE');
+            console.log(this.dataset.x2m.options);
+            if (this.dataset.x2m.options && this.dataset.x2m.options.addLabel) {
+                this.$('.oe_form_field_x2many_list_row_add a').text(this.dataset.x2m.options.addLabel)
+            }
+            // console.log(this.$('.oe_form_field_x2many_list_row_add a'));
+            // this.$('.oe_form_field_x2many_list_row_add a').text('hehe')
+            // this.$('.oe_form_field_x2many_list_row_add').remove()
+        },
+        // btn_delete_all_clicked: function() {
+        //     if (this.get('effective_readonly')) return;
+        //     this.delete_all();
+        // },
+        // delete_all: function() {
+        //     this.viewmanager.views.list.controller.do_delete(this.dataset.ids);
+        // }
+    }
+
+    core.form_widget_registry.get('many2many').include(X2ManyListDeleteAllMixin);
+    core.form_widget_registry.get('one2many').include(X2ManyListDeleteAllMixin);
+
+    return X2ManyListDeleteAllMixin;
 });

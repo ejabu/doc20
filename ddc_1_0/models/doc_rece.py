0dfdabf98e629471b1c5365a78faa95901c95418
ejabu <muhammadfahreza@gmail.com>
2017-06-06 04:14:02 +0700

    Fix Incoming Transmittal History Parent-Child Issue

--- ddc_1_0/models/doc_rece.py
+++ ddc_1_0/models/doc_rece.py
@@ -34,3 +34,82 @@ class doc_rece(models.Model):
     def oc_recv_rece_date(self):
         for rec in self.line_ids:
             rec.write({'recv_rece_date': self.recv_rece_date})
+
+
+    @api.multi
+    def _check_child_id(self):
+        child_ids=[]
+        parent_ids=[]
+        all_counter = 0
+        for amplop in self.search([]):
+            amplop_name = amplop.name
+            parent_ids.extend(amplop.line_ids.ids)
+            for line in amplop.line_ids:
+                # menghitung jumlah linked MDR
+                all_counter += 1
+                if len(line.history_ids) == 1 :
+                    # mencari ID Child
+                    child_ids.append(line.history_ids[0].id)
+
+        return parent_ids, child_ids, all_counter
+
+
+
+
+    @api.multi
+    def check_child_id(self):
+        parent_ids, child_ids, all_counter = doc_rece._check_child_id(self)
+        message = "Check berapa banyak MDR yang terhubung, namun sebenarnya Terhubung ke Parent bukan ke Child \n \n"
+        message += 'Jumlah Linked MDR : %s \n' % all_counter
+        message += 'Parent ID : %s \n' % parent_ids
+        message += '\n\n Child ID : %s \n' % child_ids
+
+        raise UserError(message)
+        return
+
+
+    @api.multi
+    def tukar_relasi(self):
+        # counter = 0
+        for amplop in self.search([]):
+            # if counter > 6:
+            #     break
+            amplop_name = amplop.name
+            child_ids_to_link = []
+            parent_ids_to_unlink = []
+            for line in amplop.line_ids:
+                # harus yang LEN == 1, karena semua duplikat sudah di delete
+                if len(line.history_ids) == 1 :
+                    child_ids_to_link.append((4, line.history_ids[0].id))
+                    parent_ids_to_unlink.append((3, line.id))
+            # import ipdb; ipdb.set_trace()
+            # amplop.write({
+            #     'line_ids':[
+            #     (   6,
+            #         0,
+            #         child_id_to_link
+            #     )
+            #     ]
+            #
+            # })
+
+            amplop.write({
+                'line_ids' : parent_ids_to_unlink
+            })
+            amplop.write({
+                'line_ids' : child_ids_to_link
+            })
+            # counter+=1
+        return
+
+
+
+
+    def _force_oc(self):
+        self.line_ids.write({'recv_trans_number': self.name})
+        self.line_ids.write({'recv_rece_date': self.recv_rece_date})
+
+    @api.multi
+    def force_oc(self):
+        for amplop in self.search([]):
+            amplop._force_oc()
